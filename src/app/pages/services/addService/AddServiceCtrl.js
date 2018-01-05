13e7819fd3511b30155bceb6b4f37e727a40d4e3
OdinsPlasmaRifle <generalaetius.jbvb@gmail.com>
2018-01-05 15:50:18 +0200

    Added hack for service terms and conditions

--- src/app/pages/services/addService/AddServiceCtrl.js
+++ src/app/pages/services/addService/AddServiceCtrl.js
@@ -64,7 +64,7 @@
 
         $scope.addServices = function(service,password){
             $scope.loadingServices = true;
-            $http.put(environmentConfig.API + '/admin/services/' + service.id + '/',{password: password,active: true}, {
+            $http.put(environmentConfig.API + '/admin/services/' + service.id + '/',{password: password, terms_and_conditions: true, active: true}, {
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': vm.token
