0ac8023d94589f2dd49cdec0d0c72a702cbc193c
AquiburRKhan <ark.aquib17@gmail.com>
2018-01-19 20:11:37 +0600

    add: added edit functionality to accounts in company setup

--- src/app/pages/newCompanySetup/setupAccounts/setupAccountsCtrl.js
+++ src/app/pages/newCompanySetup/setupAccounts/setupAccountsCtrl.js
@@ -13,14 +13,15 @@
         $rootScope.$pageFinishedLoading=true;
         $scope.account = {};
         $scope.accounts = [];
+        $scope.editingAccount= false;
         $rootScope.activeSetupRoute = 2;
 
         $scope.goToNextView=function () {
             $location.path('/company/setup/subtypes');
-        }
+        };
         $scope.goToPrevView=function () {
             $location.path('/company/setup/currency-setup');
-        }
+        };
         
         vm.initializeAccount = function () {
             $scope.account = {
@@ -53,9 +54,10 @@
                             }).then(function (res) {
                                 if (res.status === 200) {
                                     res.data.data.results.forEach(function(node){
-                                        node.group = element.name;
+                                        node.group = element;
                                     });
                                     $scope.accounts = $scope.accounts.concat(res.data.data.results);
+                                    console.log($scope.accounts)
                                     if($scope.accounts.length==0) {
                                         $rootScope.setupAccounts = 0;
                                     } else {
@@ -110,40 +112,76 @@
             }).then(function (res) {
                 if (res.status === 201) {
                     vm.initializeAccount();
-                    res.data.data.group = account.groupName.name;
+                    res.data.data.group = account.groupName;
                     $scope.accounts.push(res.data.data);
                     if($scope.accounts.length==0) {
                         $rootScope.setupAccounts = 0;
                     } else {
                         $rootScope.setupAccounts = 1;
                     }
-                    account.currencies.forEach(function(element) {
-                        $http.post(environmentConfig.API + '/admin/groups/'+ account.groupName.name +"/account-configurations/"+account.name+"/currencies/", 
-                        {
-                            "currency": element.code
-                        },
-                        {
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'Authorization': vm.token
-                            }
-                        }).then(function (res) {
-                            if (res.status === 201) {
-                                
-                            }
-                        }).catch(function (error) {
-                            $rootScope.$pageFinishedLoading = true;
-                            errorHandler.evaluateErrors(error.data);
-                            errorHandler.handleErrors(error);
-                        });
-                    })
+                    vm.addCurrenciesToAccount(account);
                 }
             }).catch(function (error) {
                 $rootScope.$pageFinishedLoading = true;
                 errorHandler.evaluateErrors(error.data);
                 errorHandler.handleErrors(error);
             });
-        }
+        };
+
+        $scope.editAccountCompanySetup = function (account) {
+            var newaccount = {
+                "name": account.name,
+                "label": account.label,
+                "primary": account.primary,
+                "default": account.default
+            };
+            $http.patch(environmentConfig.API + '/admin/groups/'+ account.groupName.name +"/account-configurations/" + account.prevName + "/", newaccount, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': vm.token
+                }
+            }).then(function (res) {
+                if (res.status === 200) {
+                    vm.initializeAccount();
+                    $scope.editingAccount = false;
+                    toastr.success('Account configuration updated');
+                    res.data.data.group = account.groupName;
+                    if($scope.accounts.length==0) {
+                        $rootScope.setupAccounts = 0;
+                    } else {
+                        $rootScope.setupAccounts = 1;
+                    }
+                    vm.addCurrenciesToAccount(account);
+                }
+            }).catch(function (error) {
+                $rootScope.$pageFinishedLoading = true;
+                errorHandler.evaluateErrors(error.data);
+                errorHandler.handleErrors(error);
+            });
+        };
+
+        vm.addCurrenciesToAccount = function (account) {
+            account.currencies.forEach(function(element) {
+                $http.post(environmentConfig.API + '/admin/groups/'+ account.groupName.name +"/account-configurations/"+account.name+"/currencies/",
+                    {
+                        "currency": element.code
+                    },
+                    {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': vm.token
+                        }
+                    }).then(function (res) {
+                    if (res.status === 201) {
+
+                    }
+                }).catch(function (error) {
+                    $rootScope.$pageFinishedLoading = true;
+                    errorHandler.evaluateErrors(error.data);
+                    errorHandler.handleErrors(error);
+                });
+            })
+        };
 
         $scope.deleteAccount = function (account) {
             $http.delete(environmentConfig.API + '/admin/groups/'+ account.group +"/account-configurations/" + account.name + "/", {
@@ -165,6 +203,14 @@
                 errorHandler.evaluateErrors(error.data);
                 errorHandler.handleErrors(error);
             });
+        }
+
+        $scope.editingAccountCompanySetup = function (account) {
+            $scope.editingAccount = true;
+            $scope.account = account;
+            $scope.account.prevName = account.name;
+            $scope.account.groupName = account.group;
+            $scope.account.currencies = [];
         }
     }
 })();
