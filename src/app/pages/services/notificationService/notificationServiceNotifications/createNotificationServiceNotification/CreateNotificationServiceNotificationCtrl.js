702170eb1f05a6166cdff729ba58f9f62ebd2c39
AquiburRKhan <ark.aquib17@gmail.com>
2018-02-22 15:08:58 +0600

    add: added new webhook event password.set

--- src/app/pages/services/notificationService/notificationServiceNotifications/createNotificationServiceNotification/CreateNotificationServiceNotificationCtrl.js
+++ src/app/pages/services/notificationService/notificationServiceNotifications/createNotificationServiceNotification/CreateNotificationServiceNotificationCtrl.js
@@ -22,6 +22,7 @@
             USER_CREATE: 'user.create',
             USER_UPDATE: 'user.update',
             USER_PASSWORD_RESET: 'user.password.reset',
+            USER_PASSWORD_SET: 'user.password.set',
             USER_EMAIL_VERIFY: 'user.email.verify',
             USER_MOBILE_VERIFY: 'user.mobile.verify',
             ADDRESS_CREATE: 'address.create',
@@ -39,7 +40,7 @@
             TRANSACTION_EXECUTE: 'transaction.execute'
         };
 
-        $scope.eventOptions = ['User Create','User Update','User Password Reset','User Email Verify','User Mobile Verify',
+        $scope.eventOptions = ['User Create','User Update','User Password Reset','User Password Set','User Email Verify','User Mobile Verify',
             'Address Create','Address Update','Document Create','Document Update',
             'Bank Account Create','Bank Account Update','Crypto Account Create','Crypto Account Update',
             'Transaction Create','Transaction Update','Transaction Delete','Transaction Initiate','Transaction Execute'];
