2f691ba4b33f4333a252590fca3e1ec7b5e50005
AquiburRKhan <ark.aquib17@gmail.com>
2018-03-23 21:07:48 +0600

    add: added account currency fees modals

--- src/app/pages/users/user/userAccountsOnly/accountSettings/accountCurrencyFees/AccountCurrencyFeesCtrl.js
+++ src/app/pages/users/user/userAccountsOnly/accountSettings/accountCurrencyFees/AccountCurrencyFeesCtrl.js
@@ -15,33 +15,8 @@
         vm.currenciesList = JSON.parse($window.sessionStorage.currenciesList);
         vm.reference = $stateParams.reference;
         $scope.userData = JSON.parse($window.sessionStorage.userData);
+        $scope.currencyObj = {};
         $scope.loadingAccountCurrencyFees = true;
-        $scope.loadingSubtypes = false;
-        $scope.editingAccountCurrencyFees = false;
-        vm.updatedAccountCurrencyFee = {};
-        $scope.accountCurrencyFeesParams = {
-            tx_type: 'Credit',
-            subtype: ''
-        };
-        $scope.txTypeOptions = ['Credit','Debit'];
-
-        $scope.getSubtypesArray = function(params,editing){
-            $scope.loadingSubtypes = true;
-            if(!editing){
-                params.subtype = '';
-            } else if(!params.subtype && editing){
-                params.subtype = '';
-            }
-            sharedResources.getSubtypes().then(function (res) {
-                res.data.data = res.data.data.filter(function (element) {
-                    return element.tx_type == (params.tx_type).toLowerCase();
-                });
-                $scope.subtypeOptions = _.pluck(res.data.data,'name');
-                $scope.subtypeOptions.unshift('');
-                $scope.loadingSubtypes = false;
-            });
-        };
-        $scope.getSubtypesArray($scope.accountCurrencyFeesParams);
 
         vm.getCurrencyObjFromCurrenciesList = function(){
             $scope.currencyObj = vm.currenciesList.find(function(element){
@@ -49,38 +24,6 @@
             });
         };
         vm.getCurrencyObjFromCurrenciesList();
-
-        $scope.toggleAccountCurrencyFeeEditView = function(accountCurrencyFee){
-            if(accountCurrencyFee) {
-                vm.getAccountCurrencyFee(accountCurrencyFee);
-            } else {
-                $scope.editAccountCurrencyFee = {};
-                $scope.getAccountCurrencyFees();
-            }
-            $scope.editingAccountCurrencyFees = !$scope.editingAccountCurrencyFees;
-        };
-
-        vm.getAccountCurrencyFee = function (accountCurrencyFee) {
-            $scope.loadingAccountCurrencyFees = true;
-            $http.get(environmentConfig.API + '/admin/accounts/' + vm.reference + '/currencies/' + vm.currencyCode + '/fees/' + accountCurrencyFee.id +'/', {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': vm.token
-                }
-            }).then(function (res) {
-                $scope.loadingAccountCurrencyFees = false;
-                if (res.status === 200) {
-                    $scope.editAccountCurrencyFee = res.data.data;
-                    $scope.editAccountCurrencyFee.value = currencyModifiers.convertFromCents($scope.editAccountCurrencyFee.value,$scope.currencyObj.divisibility);
-                    $scope.editAccountCurrencyFee.tx_type == 'credit' ? $scope.editAccountCurrencyFee.tx_type = 'Credit' : $scope.editAccountCurrencyFee.tx_type = 'Debit';
-                    $scope.getSubtypesArray($scope.editAccountCurrencyFee,'editing');
-                }
-            }).catch(function (error) {
-                $scope.loadingAccountCurrencyFees = false;
-                errorHandler.evaluateErrors(error.data);
-                errorHandler.handleErrors(error);
-            });
-        };
 
         $scope.getAccountCurrencyFees = function(){
             if(vm.token) {
@@ -104,112 +47,57 @@
         };
         $scope.getAccountCurrencyFees();
 
-        $scope.addAccountCurrencyFee = function(accountCurrencyFeesParams){
-            if(accountCurrencyFeesParams.value) {
-                if (currencyModifiers.validateCurrency(accountCurrencyFeesParams.value, $scope.currencyObj.divisibility)) {
-                    accountCurrencyFeesParams.value = currencyModifiers.convertToCents(accountCurrencyFeesParams.value, $scope.currencyObj.divisibility);
-                } else {
-                    toastr.error('Please input amount to ' + $scope.currencyObj.divisibility + ' decimal places');
-                    return;
+        $scope.openAddAccountCurrencyFeeModal = function (page, size) {
+            vm.theAddModal = $uibModal.open({
+                animation: true,
+                templateUrl: page,
+                size: size,
+                controller: 'AddAccountCurrencyFeeCtrl',
+                scope: $scope,
+                resolve: {
+                    currencyCode: function () {
+                        return vm.currencyCode;
+                    },
+                    reference: function () {
+                        return vm.reference;
+                    }
                 }
-            }
+            });
 
-            if(!accountCurrencyFeesParams.percentage){
-                accountCurrencyFeesParams.percentage = 0;
-            }
-            if(vm.token) {
-                $scope.loadingAccountCurrencyFees = true;
-                accountCurrencyFeesParams.tx_type = accountCurrencyFeesParams.tx_type.toLowerCase();
-                $http.post(environmentConfig.API + '/admin/accounts/' + vm.reference + '/currencies/' + vm.currencyCode + '/fees/',accountCurrencyFeesParams,{
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': vm.token
-                    }
-                }).then(function (res) {
-                    $scope.loadingAccountCurrencyFees = false;
-                    if (res.status === 201) {
-                        toastr.success('Fee added successfully');
-                        $scope.accountCurrencyFeesParams = {
-                            tx_type: 'Credit',
-                            subtype: ''
-                        };
-                        $scope.getSubtypesArray($scope.accountCurrencyFeesParams);
-                        $scope.getAccountCurrencyFees();
-                    }
-                }).catch(function (error) {
-                    $scope.accountCurrencyFeesParams = {
-                        tx_type: 'Credit',
-                        subtype: ''
-                    };
-                    $scope.getSubtypesArray($scope.accountCurrencyFeesParams);
-                    $scope.loadingAccountCurrencyFees = false;
-                    errorHandler.evaluateErrors(error.data);
-                    errorHandler.handleErrors(error);
-                });
-            }
-        };
-
-        $scope.accountCurrencyFeeChanged = function(field){
-            vm.updatedAccountCurrencyFee[field] = $scope.editAccountCurrencyFee[field];
-        };
-
-        $scope.updateAccountCurrencyFee = function(){
-
-            if(!$scope.editAccountCurrencyFee.subtype){
-                vm.updatedAccountCurrencyFee.subtype = '';
-            }
-
-            if($scope.editAccountCurrencyFee.value){
-                if(currencyModifiers.validateCurrency($scope.editAccountCurrencyFee.value,$scope.currencyObj.divisibility)){
-                    vm.updatedAccountCurrencyFee.value = currencyModifiers.convertToCents($scope.editAccountCurrencyFee.value,$scope.currencyObj.divisibility);
-                } else {
-                    toastr.error('Please input amount to ' + $scope.currencyObj.divisibility + ' decimal places');
-                    return;
-                }
-            } else {
-                vm.updatedAccountCurrencyFee.value = 0;
-            }
-
-            if(!vm.updatedAccountCurrencyFee.percentage){
-                vm.updatedAccountCurrencyFee.percentage = 0;
-            }
-
-            if(vm.token) {
-                $scope.loadingAccountCurrencyFees = true;
-                $scope.editingAccountCurrencyFees = !$scope.editingAccountCurrencyFees;
-                vm.updatedAccountCurrencyFee.tx_type ? vm.updatedAccountCurrencyFee.tx_type = vm.updatedAccountCurrencyFee.tx_type.toLowerCase() : '';
-
-                $http.patch(environmentConfig.API + '/admin/accounts/' + vm.reference + '/currencies/' + vm.currencyCode + '/fees/' + $scope.editAccountCurrencyFee.id +'/',vm.updatedAccountCurrencyFee,{
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': vm.token
-                    }
-                }).then(function (res) {
-                    $scope.loadingAccountCurrencyFees = false;
-                    if (res.status === 200) {
-                        toastr.success('Fee updated successfully');
-                        $scope.accountCurrencyFeesParams = {
-                            tx_type: 'Credit',
-                            subtype: ''
-                        };
-                        vm.updatedAccountCurrencyFee = {};
-                        $scope.getAccountCurrencyFees();
-                    }
-                }).catch(function (error) {
-                    $scope.accountCurrencyFeesParams = {
-                        tx_type: 'Credit',
-                        subtype: ''
-                    };
-                    vm.updatedAccountCurrencyFee = {};
+            vm.theAddModal.result.then(function(accountCurrencyFee){
+                if(accountCurrencyFee){
                     $scope.getAccountCurrencyFees();
-                    errorHandler.evaluateErrors(error.data);
-                    errorHandler.handleErrors(error);
-                });
-            }
+                }
+            }, function(){
+            });
         };
 
-        vm.findIndexOfAccountCurrencyFee = function(element){
-            return this.id == element.id;
+        $scope.openEditAccountCurrencyFeeModal = function (page, size,accountCurrencyFee) {
+            vm.theEditModal = $uibModal.open({
+                animation: true,
+                templateUrl: page,
+                size: size,
+                controller: 'EditAccountCurrencyFeeCtrl',
+                scope: $scope,
+                resolve: {
+                    accountCurrencyFee: function () {
+                        return accountCurrencyFee;
+                    },
+                    currencyCode: function () {
+                        return vm.currencyCode;
+                    },
+                    reference: function () {
+                        return vm.reference;
+                    }
+                }
+            });
+
+            vm.theEditModal.result.then(function(accountCurrencyFee){
+                if(accountCurrencyFee){
+                    $scope.getAccountCurrencyFees();
+                }
+            }, function(){
+            });
         };
 
         $scope.openAccountCurrencyFeesModal = function (page, size,accountCurrencyFee) {
@@ -224,17 +112,18 @@
                         return accountCurrencyFee;
                     },
                     currencyCode: function () {
-                        return vm.currencyCode
+                        return vm.currencyCode;
                     },
                     reference: function () {
-                        return vm.reference
+                        return vm.reference;
                     }
                 }
             });
 
             vm.theModal.result.then(function(accountCurrencyFee){
-                var index = $scope.accountCurrencyFeesList.findIndex(vm.findIndexOfAccountCurrencyFee,accountCurrencyFee);
-                $scope.accountCurrencyFeesList.splice(index, 1);
+                if(accountCurrencyFee){
+                    $scope.getAccountCurrencyFees();
+                }
             }, function(){
             });
         };
--- src/app/pages/users/user/userAccountsOnly/accountSettings/accountCurrencyFees/addAccountCurrencyFee/AddAccountCurrencyFeeCtrl.js
+++ src/app/pages/users/user/userAccountsOnly/accountSettings/accountCurrencyFees/addAccountCurrencyFee/AddAccountCurrencyFeeCtrl.js
@@ -1,26 +1,24 @@
 (function () {
     'use strict';
 
-    angular.module('BlurAdmin.pages.users.user.accountSettings.accountCurrencyLimits')
-        .controller('AddAccountCurrencyLimitCtrl', AddAccountCurrencyLimitCtrl);
+    angular.module('BlurAdmin.pages.users.user.accountSettings.accountCurrencyFees')
+        .controller('AddAccountCurrencyFeeCtrl', AddAccountCurrencyFeeCtrl);
 
-    function AddAccountCurrencyLimitCtrl($scope,$uibModalInstance,currencyCode,reference,sharedResources,$window,
+    function AddAccountCurrencyFeeCtrl($scope,$uibModalInstance,currencyCode,reference,sharedResources,$window,
                                          toastr,$http,environmentConfig,cookieManagement,currencyModifiers,errorHandler) {
 
         var vm = this;
         vm.token = cookieManagement.getCookie('TOKEN');
         vm.currencyCode = currencyCode;
-        vm.reference = reference;
         vm.currenciesList = JSON.parse($window.sessionStorage.currenciesList);
-        $scope.addingAccountCurrencyLimits = false;
+        vm.reference = reference;
         $scope.loadingSubtypes = false;
-        $scope.accountCurrencyLimitsParams = {
+        $scope.currencyObj = {};
+        $scope.accountCurrencyFeesParams = {
             tx_type: 'Credit',
-            type: 'Maximum',
             subtype: ''
         };
         $scope.txTypeOptions = ['Credit','Debit'];
-        $scope.typeOptions = ['Maximum','Maximum per day','Maximum per month','Minimum','Overdraft'];
 
         vm.getCurrencyObjFromCurrenciesList = function(){
             $scope.currencyObj = vm.currenciesList.find(function(element){
@@ -45,26 +43,31 @@
                 $scope.loadingSubtypes = false;
             });
         };
-        $scope.getSubtypesArray($scope.accountCurrencyLimitsParams);
+        $scope.getSubtypesArray($scope.accountCurrencyFeesParams);
 
-        $scope.addAccountCurrencyLimit = function(accountCurrencyLimitsParams){
-            if(accountCurrencyLimitsParams.value){
-                if(currencyModifiers.validateCurrency(accountCurrencyLimitsParams.value,$scope.currencyObj.divisibility)){
-                    accountCurrencyLimitsParams.value = currencyModifiers.convertToCents(accountCurrencyLimitsParams.value,$scope.currencyObj.divisibility);
+        $scope.addAccountCurrencyFee = function(accountCurrencyFeesParams){
+            if(accountCurrencyFeesParams.value) {
+                if (currencyModifiers.validateCurrency(accountCurrencyFeesParams.value, $scope.currencyObj.divisibility)) {
+                    accountCurrencyFeesParams.value = currencyModifiers.convertToCents(accountCurrencyFeesParams.value, $scope.currencyObj.divisibility);
                 } else {
                     toastr.error('Please input amount to ' + $scope.currencyObj.divisibility + ' decimal places');
                     return;
                 }
-            } else {
-                accountCurrencyLimitsParams.value = 0;
             }
+
+            if(!accountCurrencyFeesParams.percentage){
+                accountCurrencyFeesParams.percentage = 0;
+            }
+
+            if(accountCurrencyFeesParams.percentage > 100 || accountCurrencyFeesParams.percentage < 0){
+                toastr.error('Please input percentage value between 0 to 100');
+                return;
+            }
+
             if(vm.token) {
                 $scope.addingAccountCurrencyLimits = true;
-                accountCurrencyLimitsParams.tx_type = accountCurrencyLimitsParams.tx_type.toLowerCase();
-                accountCurrencyLimitsParams.type = accountCurrencyLimitsParams.type == 'Maximum' ? 'max': accountCurrencyLimitsParams.type == 'Maximum per day' ? 'day_max':
-                    accountCurrencyLimitsParams.type == 'Maximum per month' ? 'month_max': accountCurrencyLimitsParams.type == 'Minimum' ? 'min': 'overdraft';
-
-                $http.post(environmentConfig.API + '/admin/accounts/' + vm.reference + '/currencies/' + vm.currencyCode + '/limits/',accountCurrencyLimitsParams,{
+                accountCurrencyFeesParams.tx_type = accountCurrencyFeesParams.tx_type.toLowerCase();
+                $http.post(environmentConfig.API + '/admin/accounts/' + vm.reference + '/currencies/' + vm.currencyCode + '/fees/',accountCurrencyFeesParams,{
                     headers: {
                         'Content-Type': 'application/json',
                         'Authorization': vm.token
@@ -72,23 +75,15 @@
                 }).then(function (res) {
                     $scope.addingAccountCurrencyLimits = false;
                     if (res.status === 201) {
-                        toastr.success('Limit added successfully.');
-                        $scope.accountCurrencyLimitsParams = {
-                            tx_type: 'Credit',
-                            type: 'Maximum',
-                            subtype: ''
-                        };
-                        $scope.getSubtypesArray($scope.accountCurrencyLimitsParams);
+                        toastr.success('Fee added successfully');
                         $uibModalInstance.close(true);
                     }
                 }).catch(function (error) {
-                    $scope.accountCurrencyLimitsParams = {
+                    $scope.addingAccountCurrencyLimits = false;
+                    $scope.accountCurrencyFeesParams = {
                         tx_type: 'Credit',
-                        type: 'Maximum',
                         subtype: ''
                     };
-                    $scope.getSubtypesArray($scope.accountCurrencyLimitsParams);
-                    $scope.addingAccountCurrencyLimits = false;
                     errorHandler.evaluateErrors(error.data);
                     errorHandler.handleErrors(error);
                 });
