fe49ab84b18dc226f50be088ac447051b59a27ed
AquiburRKhan <ark.aquib17@gmail.com>
2018-04-18 17:37:44 +0600

    change: changed view after adding currencies

--- src/app/pages/currencies/addCurrencyModal/AddCurrencyModalCtrl.js
+++ src/app/pages/currencies/addCurrencyModal/AddCurrencyModalCtrl.js
@@ -12,10 +12,45 @@
         vm.token = cookieManagement.getCookie('TOKEN');
 
         $scope.currenciesToAdd = [];
-        $scope.initialCurrencies = currenciesList;
+        $scope.currenciesThatHaveBeenAdded = [];
+        $scope.initialCurrencies = currenciesList.slice();
         $scope.newCurrencyParams = {};
         $scope.showCustomCurrency = false;
+        $scope.showCurrencyAddedPanel = false;
         $scope.loadingCurrencies = false;
+
+        $scope.closeModal = function () {
+            $uibModalInstance.close(true);
+        };
+
+        vm.getCurrencies = function(){
+            $scope.loadingCurrencies = true;
+            if(vm.token){
+                $http.get(environmentConfig.API + '/admin/currencies/?enabled=true&page_size=250', {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': vm.token
+                    }
+                }).then(function (res) {
+                    if (res.status === 200) {
+                        res.data.data.results.forEach(function (currency) {
+                            var index = $scope.initialCurrencies.findIndex(function (element) {
+                                return element.code == currency.code;
+                            });
+                            if(index >=0){
+                                $scope.initialCurrencies.splice(index,1);
+                            }
+                        });
+                        $scope.loadingCurrencies = false;
+                    }
+                }).catch(function (error) {
+                    $scope.loadingCurrencies = false;
+                    errorHandler.evaluateErrors(error.data);
+                    errorHandler.handleErrors(error);
+                });
+            }
+        };
+        vm.getCurrencies();
 
         $scope.addCompanyCurrency = function (currencies) {
             if(currencies && currencies.length > 0){
@@ -30,10 +65,10 @@
                     }).then(function (res) {
                         if (res.status === 201) {
                             if(index == (array.length - 1)){
+                                $scope.currenciesThatHaveBeenAdded = currencies.slice();
                                 vm.getCompanyCurrencies();
                                 $scope.loadingCurrencies = false;
-                                toastr.success('New currencies have been created successfully');
-                                $uibModalInstance.close(true);
+                                $scope.showCurrencyAddedPanel = true;
                             }
                         }
                     }).catch(function (error) {
@@ -61,11 +96,11 @@
                 }
             }).then(function (res) {
                 $scope.loadingCurrencies = false;
+                $scope.currenciesThatHaveBeenAdded.push(newCurrencyParams);
                 if (res.status === 201) {
                     vm.getCompanyCurrencies();
                     $scope.loadingCurrencies = false;
-                    toastr.success('New custom currency has been created successfully');
-                    $uibModalInstance.close(true);
+                    $scope.showCurrencyAddedPanel = true;
                 }
             }).catch(function (error) {
                 $scope.loadingCurrencies = false;
