f32d41b54166abe6416189812674e8f5930299d3
AquiburRKhan <ark.aquib17@gmail.com>
2018-05-11 17:04:12 +0600

    change: updated user permissions

--- src/app/pages/users/user/userDetails/permissions/userPermissions/UserPermissionsCtrl.js
+++ src/app/pages/users/user/userDetails/permissions/userPermissions/UserPermissionsCtrl.js
@@ -17,71 +17,73 @@
         $scope.typeOptionsObj = {
             ACCOUNT : 'account',
             ACCOUNT_CURRENCY : 'accountcurrency',
+            ACCOUNT_CURRENCY_FEE : 'accountcurrencyfee',
             ACCOUNT_CURRENCY_LIMIT : 'accountcurrencylimit',
-            ACCOUNT_CURRENCY_TRANSACTION_FEE : 'accountcurrencytransactionfee',
-            ACCOUNT_CURRENCY_TRANSACTION_SWITCH : 'accountcurrencytransactionswitch',
+            CURRENCY : 'currency',
+            BANK_BRANCH_ADDRESS : 'bankbranchaddress',
             COMPANY : 'company',
             COMPANY_ADDRESS : 'companyaddress',
             COMPANY_BANK_ACCOUNT : 'companybankaccount',
-            COMPANY_TRANSACTION_SWITCH : 'companytransactionswitch',
+            COMPANY_NOTIFICATION : 'companynotification',
+            COMPANY_SERVICE : 'companyservice',
             CRYPTO_ACCOUNT : 'cryptoaccount',
-            CURRENCY : 'currency',
-            CURRENCY_TIER : 'currencytier',
-            CURRENCY_TIER_LIMIT : 'currencytierlimit',
-            CURRENCY_TIER_REQUIREMENT : 'currencytierrequirement',
-            CURRENCY_TIER_TRANSACTION_FEE : 'currencytiertransactionfee',
-            CURRENCY_TIER_TRANSACTION_SWITCH : 'currencytiertransactionswitch',
             DOCUMENT : 'document',
-            EMAIL_ADDRESS : 'emailaddress',
-            GENERAL_WEBHOOK : 'generalwebhook',
-            GLOBAL_SWITCH : 'globalswitch',
-            MOBILE : 'mobile',
-            NOTIFICATION : 'notification',
-            PERMISSION : 'permission',
-            COMPANY_PERMISSION_GROUP : 'companypermissiongroup',
-            AUTH_TOKEN : 'authtoken',
-            TRANSACTION : 'transaction',
-            TRANSACTION_SUBTYPE : 'transactionsubtype',
-            TRANSACTION_WEBHOOK : 'transactionwebhook',
             USER : 'user',
             USER_ADDRESS : 'useraddress',
             USER_BANK_ACCOUNT : 'userbankaccount',
-            USER_TRANSACTION_SWITCH : 'usertransactionswitch'
+            MOBILE : 'mobile',
+            GROUP: 'group',
+            GROUP_TIER: 'grouptier',
+            GROUP_TIER_FEE: 'grouptierfee',
+            GROUP_TIER_LIMIT: 'grouptierlimit',
+            GROUP_TIER_REQUIREMENT: 'grouptierrequirement',
+            ACCOUNT_CONFIGURATION: 'accountconfiguration',
+            REQUEST : 'request',
+            SERVICE : 'service',
+            TRANSACTION : 'transaction',
+            TRANSACTION_FEE : 'transactionfee',
+            TRANSACTION_MESSAGE : 'transactionmessage',
+            TRANSACTION_SUBTYPE : 'transactionsubtype',
+            TRANSFER : 'transfer',
+            WEBHOOK : 'webhook',
+            WEBHOOK_REQUEST : 'webhookrequest',
+            WEBHOOK_TASK : 'webhooktask'
         };
 
         $scope.typeOptions = [
             {type:'Account',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
             {type:'Account currency',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Account currency fee',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
             {type:'Account currency limit',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Account currency transaction fee',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Account currency transaction switch',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Currency',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Bank branch address',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
             {type:'Company',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
             {type:'Company address',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
             {type:'Company bank account',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Company transaction switch',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Company notification',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Company service',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
             {type:'Crypto account',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Currency',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Currency tier',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Currency tier limit',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Currency tier requirement',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Currency tier transaction fee',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Currency tier transaction switch',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
             {type:'Document',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Email address',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'General webhook',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Global switch',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Mobile',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Notification',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Permission',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Company permission group',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Auth token',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Transaction',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Transaction subtype',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'Transaction webhook',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
             {type:'User',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
             {type:'User address',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
             {type:'User bank account',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
-            {type:'User transaction switch',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]}
+            {type:'Mobile',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Group',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Group tier',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Group tier fee',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Group tier limit',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Group tier requirement',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Account configuration',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Request',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Service',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Transaction',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Transaction fee',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Transaction message',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Transaction subtype',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Transfer',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Webhook',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Webhook request',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]},
+            {type:'Webhook task',levels: [{name: 'view',enabled: false},{name: 'add',enabled: false},{name: 'change',enabled: false},{name: 'delete',enabled: false}]}
         ];
 
 
@@ -116,9 +118,9 @@
                                 $scope.typeOptions[typeOptionsIndex].levels[levelIndex].enabled = true;
                                 $scope.typeOptions[typeOptionsIndex].levels[levelIndex].id = permissions[index].id;
                             }
-                        })
+                        });
                     }
-                })
+                });
             });
         };
 
@@ -133,7 +135,7 @@
                 }
 
                 index = vm.checkedLevels.findIndex(function (element) {
-                    return (element.type == typeOption.type && element.level == level.name)
+                    return (element.type == typeOption.type && element.level == level.name);
                 });
 
                 return index;
@@ -145,19 +147,19 @@
                 var index = findIndexOfLevel();
                 if(index > -1){
                     vm.checkedLevels.splice(index,1);
-                    return
+                    return;
                 } else {
                     vm.checkedLevels.push({type: typeOption.type,level: level.name,id: level.id});
-                    return
+                    return;
                 }
             } else if(!level.enabled && level.id){
                 var index = findIndexOfLevel();
                 if(index > -1){
                     vm.checkedLevels.splice(index,1);
-                    return
+                    return;
                 } else {
                     vm.checkedLevels.push({type: typeOption.type,level: level.name,id: level.id});
-                    return
+                    return;
                 }
             }
 
@@ -179,7 +181,7 @@
                 type = type.replace(/ /g, '_');
                 if(idx === array.length - 1){
                     if(element.id){
-                        vm.deletePermission(element,'last')
+                        vm.deletePermission(element,'last');
                     } else {
                         vm.addPermissions({type: $scope.typeOptionsObj[type],level: element.level},'last');
                     }
@@ -188,7 +190,7 @@
                 }
 
                 if(element.id){
-                    vm.deletePermission(element)
+                    vm.deletePermission(element);
                 } else {
                     vm.addPermissions({type: $scope.typeOptionsObj[type],level: element.level});
                 }
