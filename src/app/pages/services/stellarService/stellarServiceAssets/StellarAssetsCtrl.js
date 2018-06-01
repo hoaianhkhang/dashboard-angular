3dfcfa783625dad2ba2734cce44b5ce42179b4f3
AquiburRKhan <ark.aquib17@gmail.com>
2018-06-01 13:46:45 +0600

    change: added balance field and changed assets UI

--- src/app/pages/services/stellarService/stellarServiceAssets/StellarAssetsCtrl.js
+++ src/app/pages/services/stellarService/stellarServiceAssets/StellarAssetsCtrl.js
@@ -5,17 +5,16 @@
         .controller('StellarAssetsCtrl', StellarAssetsCtrl);
 
     /** @ngInject */
-    function StellarAssetsCtrl($scope,localStorageManagement,currenciesList,$http,errorHandler,toastr,sharedResources,
-                                  $uibModal,currencyModifiers,serializeFiltersService,environmentConfig) {
-        $scope.stellarAccountSettingView = '';
+    function StellarAssetsCtrl($scope,localStorageManagement,currenciesList,$http,errorHandler,$filter,$uibModal) {
 
         var vm = this;
         vm.serviceUrl = localStorageManagement.getValue('SERVICEURL');
         vm.token = localStorageManagement.getValue('TOKEN');
         $scope.stellarCurrency = currenciesList.find(function (element) {
-            return element.code == 'XLM';
+        return element.code == 'XLM';
         });
-        $scope.assetsObjLength = 0;
+        $scope.assetsObj = [];
+        $scope.stellarAccountSettingView = '';
 
         vm.getAssets = function () {
             $scope.loadingAssets =  true;
@@ -26,19 +25,46 @@
                         'Authorization': vm.token
                     }
                 }).then(function (res) {
-                    $scope.loadingAssets =  false;
                     if (res.status === 200) {
-                        $scope.assetsObj = res.data.data;
-                        console.log($scope.assetsObj)
-                        $scope.assetsObjLength = Object.keys($scope.assetsObj).length;
+                        res.data.data.forEach(function (asset,index,array) {
+                            if(index === array.length - 1){
+                                vm.getAssetsBalance(asset,'last');
+                            } else {
+                                vm.getAssetsBalance(asset);
+                            }
+                        });
                     }
                 }).catch(function (error) {
+                    $scope.loadingAssets =  false;
                     errorHandler.evaluateErrors(error.data);
                     errorHandler.handleErrors(error);
                 });
             }
         };
         vm.getAssets();
+
+        vm.getAssetsBalance = function (asset,last) {
+            if(asset){
+                $http.get(vm.serviceUrl + 'admin/asset/' + asset.id + '/', {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': vm.token
+                    }
+                }).then(function (res) {
+                    if (res.status === 200) {
+                        asset.balance = $filter("currencyModifiersFilter")(res.data.data.rehive_acc_balance.available_balance,res.data.data.rehive_acc_balance.currency.divisibility);
+                        $scope.assetsObj.push(asset);
+                        if(last){
+                            $scope.loadingAssets =  false;
+                        }
+                    }
+                }).catch(function (error) {
+                    $scope.loadingAssets =  false;
+                    errorHandler.evaluateErrors(error.data);
+                    errorHandler.handleErrors(error);
+                });
+            }
+        };
 
         $scope.openAddAssetsModal = function (page, size) {
             vm.theModal = $uibModal.open({
