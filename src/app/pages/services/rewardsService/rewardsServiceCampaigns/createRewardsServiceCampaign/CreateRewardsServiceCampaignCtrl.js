57900ebc81742a249313010d35755cbbf0a8fa44
AquiburRKhan <ark.aquib17@gmail.com>
2018-07-17 19:11:29 +0600

    add: started adding ng-tags-input

--- src/app/pages/services/rewardsService/rewardsServiceCampaigns/createRewardsServiceCampaign/CreateRewardsServiceCampaignCtrl.js
+++ src/app/pages/services/rewardsService/rewardsServiceCampaigns/createRewardsServiceCampaign/CreateRewardsServiceCampaignCtrl.js
@@ -5,14 +5,60 @@
         .controller('CreateRewardsServiceCampaignsCtrl', CreateRewardsServiceCampaignsCtrl);
 
     /** @ngInject */
-    function CreateRewardsServiceCampaignsCtrl($scope,$rootScope,$http,localStorageManagement,$location,$uibModal,errorHandler) {
+    function CreateRewardsServiceCampaignsCtrl($scope,environmentConfig,typeaheadService,
+                                               $http,localStorageManagement,$location,errorHandler) {
 
         var vm = this;
         vm.token = localStorageManagement.getValue('TOKEN');
         vm.baseUrl = localStorageManagement.getValue('SERVICEURL');
-
+        $scope.currencyOptions = [];
+        $scope.userOptions = [];
         $scope.addingCampaign =  false;
-        $scope.newCampaignParams = {};
+        $scope.newCampaignParams = {
+            currencies: [],
+            users: []
+        };
+
+        $scope.pushUser = function () {
+            $scope.newCampaignParams.users.push('sadsad');
+            console.log($scope.newCampaignParams)
+        };
+
+        //for angular datepicker
+        $scope.dateObj = {};
+        $scope.dateObj.format = 'MM/dd/yyyy';
+        $scope.popup1 = {};
+        $scope.open1 = function() {
+            $scope.popup1.opened = true;
+        };
+
+        $scope.popup2 = {};
+        $scope.open2 = function() {
+            $scope.popup2.opened = true;
+        };
+
+        $scope.getUsersEmailTypeahead = typeaheadService.getUsersEmailTypeahead();
+
+        vm.getCompanyCurrencies = function(){
+            if(vm.token){
+                $http.get(environmentConfig.API + '/admin/currencies/?enabled=true&page_size=250', {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': vm.token
+                    }
+                }).then(function (res) {
+                    if (res.status === 200) {
+                        if(res.data.data.results.length > 0){
+                            $scope.currencyOptions = res.data.data.results;
+                        }
+                    }
+                }).catch(function (error) {
+                    errorHandler.evaluateErrors(error.data);
+                    errorHandler.handleErrors(error);
+                });
+            }
+        };
+        vm.getCompanyCurrencies();
 
 
     }
--- src/app/pages/transactions/history/HistoryCtrl.js
+++ src/app/pages/transactions/history/HistoryCtrl.js
@@ -222,6 +222,8 @@
             $scope.popup2.opened = true;
         };
 
+        //end angular datepicker
+
         $scope.orderByFunction = function () {
             return ($scope.applyFiltersObj.orderByFilter.selectedOrderByOption == 'Latest' ? '-created' :
                 $scope.applyFiltersObj.orderByFilter.selectedOrderByOption == 'Largest' ? '-amount' :
