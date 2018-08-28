cd7cf82e3d1376fb9bfbbc109a25cd4abdd218c8
AquiburRKhan <ark.aquib17@gmail.com>
2018-08-28 15:18:33 +0600

    fix: fixed multifactor auth

--- src/app/pages/multiFactorAuth/multiFactorAuthVerify/MultiFactorAuthVerifyCtrl.js
+++ src/app/pages/multiFactorAuth/multiFactorAuthVerify/MultiFactorAuthVerifyCtrl.js
@@ -17,7 +17,7 @@
         vm.getTokenAuthenticationDetails = function(){
             if(vm.token) {
                 $scope.loadingVerifyAuth = true;
-                Rehive.auth.mfa.token.get().then(function (res) {
+                Rehive.auth.mfa.token.enable().then(function (res) {
                     $scope.tokenAuthenticationDetails = res;
                     $scope.qrCodeUrl = 'https://chart.googleapis.com/chart?cht=qr&chl='+
                         encodeURIComponent(res.otpauth_url) + '&chs=200x200&chld=L|0';
