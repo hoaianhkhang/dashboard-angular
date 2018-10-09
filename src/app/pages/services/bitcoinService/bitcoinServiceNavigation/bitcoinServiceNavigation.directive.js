d0e3d1620fcfd4c1bfe2e59455103a8f01c822c4
AquiburRKhan <ark.aquib17@gmail.com>
2018-10-09 20:20:09 +0600

    add: added TXBT when bitcoin testnet service is entered

--- src/app/pages/services/bitcoinService/bitcoinServiceNavigation/bitcoinServiceNavigation.directive.js
+++ src/app/pages/services/bitcoinService/bitcoinServiceNavigation/bitcoinServiceNavigation.directive.js
@@ -9,10 +9,16 @@
         return {
             restrict: 'E',
             templateUrl: 'app/pages/services/bitcoinService/bitcoinServiceNavigation/bitcoinServiceNavigation.html',
-            controller: function($rootScope,$scope,$location){
+            controller: function($rootScope,$scope,$location,localStorageManagement){
                 var location = $location.path();
                 var locationArray = location.split('/');
                 $scope.locationIndicator = locationArray[(locationArray.length -1)];
+                var serviceUrl = localStorageManagement.getValue('SERVICEURL');
+                if(serviceUrl.indexOf('bitcoin-testnet') > 0){
+                    $scope.inTestnetService = true;
+                } else {
+                    $scope.inTestnetService = false;
+                }
             }
         };
     }
