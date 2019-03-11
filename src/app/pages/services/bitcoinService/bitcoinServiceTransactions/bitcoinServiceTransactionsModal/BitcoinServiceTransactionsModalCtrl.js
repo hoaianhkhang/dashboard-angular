fbdaec3f5f779d18014e443d9a864ec2a180c6a7
Dblaze47 <manosh.office015@gmail.com>
2019-03-11 13:30:39 +0600

    Fix: Updated the Bitcoin transaction blocktrail urls (testnet and mainnet)

--- src/app/pages/services/bitcoinService/bitcoinServiceTransactions/bitcoinServiceTransactionsModal/BitcoinServiceTransactionsModalCtrl.js
+++ src/app/pages/services/bitcoinService/bitcoinServiceTransactions/bitcoinServiceTransactionsModal/BitcoinServiceTransactionsModalCtrl.js
@@ -12,11 +12,9 @@
         $scope.transaction = transaction;
         vm.serviceUrl = localStorageManagement.getValue('SERVICEURL');
         if(vm.serviceUrl.indexOf('testnet') > 0){
-            $scope.bitcoinBlockTrailUrl = 'https://live.blockcypher.com/bcy/';
-            // $scope.bitcoinBlockTrailUrl = 'https://www.blocktrail.com/tBTC';
+            $scope.bitcoinBlockTrailUrl = 'https://live.blockcypher.com/btc-testnet';
         } else {
-            $scope.bitcoinBlockTrailUrl = 'https://live.blockcypher.com/';
-            // $scope.bitcoinBlockTrailUrl = 'https://www.blocktrail.com/BTC';
+            $scope.bitcoinBlockTrailUrl = 'https://live.blockcypher.com/btc';
         }
 
         $scope.goToUser = function () {
